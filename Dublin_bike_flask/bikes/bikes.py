275a01d95b0bce716b2b182fa759d4322c880737
andylight0528@gmail.com <andylight0528@gmail.com>
2020-03-15 19:53:17 +0000

    delete redundancy

--- Dublin_bike_flask/bikes/bikes.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint, render_template
-
-bike_bp = Blueprint('bike_bp', __name__,template_folder='templates', static_folder="static",
-                    static_url_path="/bikes/static")
-
-@bike_bp.route("/")
-def bike():
-    return render_template("index.html")
