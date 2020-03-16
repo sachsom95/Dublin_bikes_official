a16f4d73572689360d42e399d123a242e5219eed
andylight0528@gmail.com <andylight0528@gmail.com>
2020-03-16 19:03:24 +0000

    1.add event; 2.ajax to update page info by accessing DB

--- bike_flask/bikes/bikes.py
+++ bike_flask/bikes/bikes.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, render_template, send_file, json
+from flask import Blueprint, render_template, send_file, json,request
 from dublin_bike import data_get
+from model import get_station_info
 
 
 bike_bp = Blueprint('bike_bp', __name__,template_folder='templates', static_folder="static",
@@ -14,3 +15,21 @@ def bike():
 def bike_data():
     json_data = data_get()
     return json.dumps(json_data)
+
+
+@bike_bp.route("/station")
+def station_info():
+    lat = request.args.get("lat")
+    lng = request.args.get("lng")
+    # print(lat, lng)
+
+    station_info = get_station_info(lat,lng)
+    print(type(station_info), station_info)
+    return station_info
+
+
+# select bike_stands, available_bike_stands, available_bikes, status
+# from status
+# join position
+# using (number)
+# where latitude = 53.349013 and longitude = -6.260311;
--- bike_flask/model.py
+++ bike_flask/model.py
@@ -1,21 +1,30 @@
 from sqlalchemy import *
+from flask import json
 
-db_uri = "mysql://root:lhb280214@localhost:3306/dublin_bike"
-engine = create_engine(db_uri)
+def get_station_info(lat,lng):
+    db_uri = "mysql://root:lhb280214@localhost:3306/dublin_bike"
+    engine = create_engine(db_uri)
+    conn = engine.connect()
+# metadata = MetaData()
+# inspector = inspect(engine)
+# print(inspector.get_table_names()) # 获取数据库的所有table信息
+# print(inspector.get_columns('position'))  #获取某个table的所有column
+# print("*********")
+    sql = "select bike_stands, available_bike_stands, available_bikes, status " \
+          "from status " \
+          "join position " \
+          "using (number) " \
+          "where latitude = " + str(lat) + " and longitude = " + str(lng) + ";"
+    result = conn.execute(sql)
+    station_dict = {}
+    for row in result:
+        station_dict['bike_stands'] = row['bike_stands']
+        station_dict['available_bike_stands'] = row['available_bike_stands']
+        station_dict['available_bikes'] = row['available_bikes']
+        station_dict['status'] = row['status']
+    return json.dumps(station_dict)
 
-metadata = MetaData()
-# print(metadata)
-# census = Table('census', metadata, autoload=True, autoload_with=engine)
 
-inspector = inspect(engine)
-print(inspector.get_table_names()) # 获取数据库的所有table信息
-print(inspector.get_columns('position'))  #获取某个table的所有column
-print("*********")
-sql = "select * from position "
-result = engine.execute(sql)
-
-print("result is",result)
-print(type(result))
 
 # for r in result:
 #   print(r)
