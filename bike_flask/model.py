e178e70de6dc04169ce8c80a172c9c20b7896d5f
andylight0528@gmail.com <andylight0528@gmail.com>
2020-03-17 11:58:32 +0000

    update key and data

--- bike_flask/model.py
+++ bike_flask/model.py
@@ -1,15 +1,39 @@
 from sqlalchemy import *
 from flask import json
 
-def get_station_info(lat,lng):
+def get_conn():
     db_uri = "mysql://root:lhb280214@localhost:3306/dublin_bike"
     engine = create_engine(db_uri)
     conn = engine.connect()
-# metadata = MetaData()
-# inspector = inspect(engine)
-# print(inspector.get_table_names()) # 获取数据库的所有table信息
-# print(inspector.get_columns('position'))  #获取某个table的所有column
-# print("*********")
+    return conn
+
+def update():
+    conn = get_conn()
+    sql = ""
+
+def station_init():
+    conn = get_conn()
+    sql = "select number, name, address, latitude, longitude from position"
+    result = conn.execute(sql)
+    station_list = []
+    for row in result:
+        single_station = {}
+        for each in row:
+            single_station['number'] = row['number']
+            single_station['name'] = row['name']
+            single_station['address'] = row['address']
+            single_station['latitude'] = float(row['latitude'])
+            single_station['longitude'] = float(row['longitude'])
+        station_list.append(single_station)
+    return json.dumps(station_list)
+# a = station_init()
+# print(a)
+
+def get_station_info(lat,lng):
+    # db_uri = "mysql://root:dublinbikes@dublin.cwl57f78tvsl.us-east-1.rds.amazonaws.com:3306/bikes"
+    db_uri = "mysql://root:lhb280214@localhost:3306/dublin_bike"
+    engine = create_engine(db_uri)
+    conn = engine.connect()
     sql = "select bike_stands, available_bike_stands, available_bikes, status " \
           "from status " \
           "join position " \
