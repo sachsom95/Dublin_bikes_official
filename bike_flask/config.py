8e377439b5157d3445a9449955aa202c6424eab7
FreddieNIU <60135324+FreddieNIU@users.noreply.github.com>
2020-04-14 12:48:45 +0100

    clean master branch

--- bike_flask/config.py
+++
@@ -1,7 +0,0 @@
-
-class Config():
-    def __init__(self, host = 'localhost', database = 'dublin_bike', username = 'root', password = 'lhb280214'):
-        self.host = host
-        self.database = database
-        self.username = username
-        self.password = password
